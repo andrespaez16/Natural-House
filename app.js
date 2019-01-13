781e31cb97d1cc185cd7f054dd19e9f5431f15f9
andrespaez16 <kinkbmx216@gmail.com>
2019-01-12 19:52:19 -0500

    etiquetas hot y se arreglas el search y iconos

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-(function($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
+ 
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -40,11 +37,4 @@
   $(window).scroll(navbarCollapse);
 
   // Hide navbar when modals trigger
-  $('.portfolio-modal').on('show.bs.modal', function(e) {
-    $('.navbar').addClass('d-none');
-  })
-  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
-    $('.navbar').removeClass('d-none');
-  })
-
-})(jQuery); // End of use strict
+  
